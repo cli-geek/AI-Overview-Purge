c12faf8c87bf470651d6359df33258346ac5d958
cli-geek <austinlaubach@outlook.com>
2026-04-08 01:40:28 -0400

    created base js files, and new icons for 16x16, 48x48, and 128x128 resolutions

---
+++ background.js
@@ -0,0 +1,4 @@
+// Service worker file - handles declarativeNetRequest rules
+if (typeof importScripts === 'function') {
+    importScripts('./browser-polyfill.js'); // Imports the polyfille lib for Chrome
+}
---
+++ popup.js
@@ -0,0 +1,1 @@
+// Reads/writes toggle state and updates rules
