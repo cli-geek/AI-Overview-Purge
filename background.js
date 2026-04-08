7f371d6bece54daba3a7e67ac02a3a23a9057302
cli-geek <austinlaubach@outlook.com>
2026-04-08 01:51:30 -0400

    began writing background.js. imports mozilla's polyfill for cross-compatiblity between chrome and firefox. wrote a basic first RULE, and two functions to handle the enable/disable behavior.

--- background.js
+++ background.js
@@ -2,3 +2,31 @@
 if (typeof importScripts === 'function') {
     importScripts('./browser-polyfill.js'); // Imports the polyfille lib for Chrome
 }
+
+const RULES = [
+    {
+        id: 1,
+        priority: 1,
+        action: {
+            type: "redirect",
+            redirect: { transform: { queryTransform: { addOrReplaceParams: [{ key: "q", value: "placeholder"}] } } }
+        },
+        condition: {
+            urlFilter: "*://www.google.com/search*",
+            resourceTypes: ["main_frame"]
+        }
+    }
+];
+
+async function enabledRules() { // called when toggled ON
+    await browser.declarativeNetRequest.updateDynamicRules({
+        addRules: RULES,
+        removeRuleIds: RULES.map(rule => rule.id)
+    });
+}
+
+async function disableRules() { // called when toggled OFF
+    await browser.declarativeNetRequest.updateDynamicRules({
+        removeRuleIds: RULES.map(rule => rule.id)
+    });
+}
